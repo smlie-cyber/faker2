3fc151c675c5dafbcd5ad94ffc4c69bd3fcc64f8
shufflewzc <574427728@qq.com>
2021-07-08 11:20:11 +0800

    Update jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -11,7 +11,7 @@ https://raw.githubusercontent.com/smiek2221/scripts/master/MovementFaker.js
 
 */
 const $ = new Env('燃动夏季');
-const MovementFaker = require('./jd_MovementFaker.js')
+const MovementFaker = require('./shufflewzc_faker2_jd_MovementFaker.js')
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
