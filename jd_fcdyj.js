05c68f5e6fffb03b3bf1222f18db999a176bfa55
shufflewzc <574427728@qq.com>
2021-11-04 10:51:34 +0800

    update

--- jd_fcdyj.js
+++ jd_fcdyj.js
@@ -66,9 +66,9 @@ const JD_API_HOST = `https://api.m.jd.com`;
             console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
         }
         if (!dyjCode) {
-            console.log(`\n环境变量中没有检测到助力码,开始获取【京东账号${$.index}】助力码\n`)
             await open()
             if ($.hotFlag) continue;
+            console.log(`\n环境变量中没有检测到助力码,开始获取【京东账号${$.index}】助力码\n`)
             await getid()
         } else {
             dyjStr = dyjCode.split("@")
@@ -93,7 +93,7 @@ const JD_API_HOST = `https://api.m.jd.com`;
                 cookie = cookiesArr[i];
                 $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
                 $.canRun = true
-                console.log(`\n${$.UserName} 剩余助力去助力作者\n`)
+                console.log(`\n${$.UserName} 去助力【zero205】\n`)
                 for (let j = 0; j < $.authorCode.length; j++) {
                     let item = $.authorCode[j];
                     await help(item.redEnvelopeId, item.inviter, 1)
@@ -141,10 +141,12 @@ async function exchange() {
                     console.log(`${$.name} API请求失败，请检查网路重试`);
                 } else {
                     data = JSON.parse(data);
-                    if (data.success && data.data.chatEnvelopeVo.status == 50059) {
-                        console.log(`【京东账号${$.index}】${data.data.chatEnvelopeVo.message} ，尝试兑换红包...`)
-                        $.rewardType = 1
-                        await exchange()
+                    if (data.success && data.data) {
+                        if (data.data.chatEnvelopeVo.status == 50053 || data.data.chatEnvelopeVo.status == 50059) {
+                            console.log(`【京东账号${$.index}】${data.data.chatEnvelopeVo.message} ，尝试兑换红包...`)
+                            $.rewardType = 1
+                            await exchange()
+                        }
                     } else {
                         console.log(`【京东账号${$.index}】提现成功`)
                     }
@@ -317,7 +319,7 @@ function help(rid, inviter, type) {
 function getAuthorShareCode() {
     return new Promise(resolve => {
         $.get({
-            url: "https://raw.fastgit.org/shufflewzc/updateTeam/main/shareCodes/dyj.json",
+            url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/dyj.json",
             headers: {
                 "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
             }
--- jd_superBrand.js
+++ jd_superBrand.js
@@ -1,10 +1,11 @@
 /*
 双11特务
 APP首页下滑,任务，互助
-cron 10 9,13,16,19 2-8 11 * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_superBrand.js
+cron 10 9,13,16,19,20 2-8 11 * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_superBrand.js
 * */
 const $ = new Env('双11特务');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
@@ -95,7 +96,7 @@ let shareList = [];
 function getShareCode(name) {
   return new Promise(resolve => {
     $.get({
-      url: "https://gitee.com/KingRan521/JD-Scripts/raw/master/shareCodes/"+name,
+      url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/"+name,
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
@@ -105,7 +106,7 @@ function getShareCode(name) {
           console.log(`${JSON.stringify(err)}`);
           console.log(`${$.name} API请求失败，请检查网路重试`);
         } else {
-          console.log(`优先账号内部互助`);
+          console.log(`优先账号内部互助，有剩余助力次数再帮【zero205】助力`);
           $.shareCode = JSON.parse(data);
         }
       } catch (e) {
@@ -176,19 +177,53 @@ async function main(cookie) {
             console.log(`执行结果：${JSON.stringify(doInfo)}`);
             await $.wait(3000);
         }
-
+        if(oneTask.assignmentType === 3){
+            console.log(`任务：${oneTask.assignmentName},去执行,请稍稍`);
+            let itemId = oneTask.ext.followShop[0].itemId || '';
+            if(!itemId){
+                console.log(`任务：${oneTask.assignmentName},信息异常`);
+            }
+            let doInfo = await takeRequest(cookie,'superBrandDoTask',`{"source":"card","activityId":${activityId},"encryptProjectId":"${encryptProjectId}","encryptAssignmentId":"${oneTask.encryptAssignmentId}","assignmentType":${oneTask.assignmentType},"itemId":"${itemId}","actionType":0}`);
+            console.log(`执行结果：${JSON.stringify(doInfo)}`);
+            await $.wait(3000);
+        }
+        if(oneTask.assignmentType === 7){
+            console.log(`任务：${oneTask.assignmentName},去执行,请稍稍`);
+            let itemId = oneTask.ext.brandMemberList[0].itemId || '';
+            if(!itemId){
+                console.log(`任务：${oneTask.assignmentName},信息异常`);
+            }
+            let doInfo = await takeRequest(cookie,'superBrandDoTask',`{"source":"card","activityId":${activityId},"encryptProjectId":"${encryptProjectId}","encryptAssignmentId":"${oneTask.encryptAssignmentId}","assignmentType":${oneTask.assignmentType},"itemId":"${itemId}","actionType":0}`);
+            console.log(`执行结果：${JSON.stringify(doInfo)}`);
+            await $.wait(3000);
+        }
         if(oneTask.assignmentType === 5){
             let signList = oneTask.ext.sign2 || [];
             if(signList.length === 0){
                 console.log(`任务：${oneTask.assignmentName},信息异常`);
             }
-            for (let j = 0; j < signList.length; j++) {
-                if(signList[j].status === 1){
-                    console.log(`任务：${oneTask.assignmentName},去执行,请稍稍`);
-                    let itemId = signList[j].itemId;
-                    let doInfo = await takeRequest(cookie,'superBrandDoTask',`{"source":"card","activityId":${activityId},"encryptProjectId":"${encryptProjectId}","encryptAssignmentId":"${oneTask.encryptAssignmentId}","assignmentType":${oneTask.assignmentType},"itemId":"${itemId}","actionType":0,"dropDownChannel":1}`);
-                    console.log(`执行结果：${JSON.stringify(doInfo)}`);
-                    await $.wait(3000);
+            if(oneTask.assignmentName === '首页限时下拉'){
+                for (let j = 0; j < signList.length; j++) {
+                    if(signList[j].status === 1){
+                        console.log(`任务：${oneTask.assignmentName},去执行,请稍稍`);
+                        let itemId = signList[j].itemId;
+                        let doInfo = await takeRequest(cookie,'superBrandDoTask',`{"source":"card","activityId":${activityId},"encryptProjectId":"${encryptProjectId}","encryptAssignmentId":"${oneTask.encryptAssignmentId}","assignmentType":${oneTask.assignmentType},"itemId":"${itemId}","actionType":0,"dropDownChannel":1}`);
+                        console.log(`执行结果：${JSON.stringify(doInfo)}`);
+                        await $.wait(3000);
+                    }
+                }
+            }else if(oneTask.assignmentName === '去首页下拉参与小游戏'){
+                for (let j = 0; j < signList.length; j++) {
+                    if(signList[j].status === 1){
+                        console.log(`任务：${oneTask.assignmentName},去执行,请稍稍`);
+                        let gameInfo = await takeRequest(cookie,'showSecondFloorGameInfo',`{"source":"card"}`);
+                        let secCode = gameInfo.result.activityGameInfo.gameCurrentRewardInfo.secCode;
+                        let gameEncryptAssignmentId = gameInfo.result.activityGameInfo.gameCurrentRewardInfo.encryptAssignmentId;
+                        await $.wait(3000);
+                        let doInfo = await takeRequest(cookie,'superBrandTaskLottery',`{"source":"card","activityId":${activityId},"encryptProjectId":"${encryptProjectId}","encryptAssignmentId":"${gameEncryptAssignmentId}","secCode":"${secCode}"}`);
+                        console.log(`执行结果：${JSON.stringify(doInfo)}`);
+                        await $.wait(3000);
+                    }
                 }
             }
         }
