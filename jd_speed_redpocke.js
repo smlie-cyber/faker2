d19ad6efea182f43cafd5043ccca62daac47a412
shufflewzc <574427728@qq.com>
2021-11-18 16:46:50 +0800

    Update jd_speed_redpocke.js

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -396,8 +396,9 @@ function cashOut(id,poolBaseId,prizeGroupId,prizeBaseId,) {
 function invite() {
   let t = +new Date()
   let inviterId = [
-    "5V7vHE23qh2EkdBHXRFDuA=="
-  ][Math.floor((Math.random() * 1))]
+    "5V7vHE23qh2EkdBHXRFDuA==",
+    "wXX9SjXOdYMWe5Ru/1+x9A=="
+  ][Math.floor((Math.random() * 2))]
   var headers = {
     'Host': 'api.m.jd.com',
     'accept': 'application/json, text/plain, */*',
